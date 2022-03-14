af43deaeb59560874b08e7d737b066a3daebfb3b
Ravi <ravi.souza@gmail.com>
2022-03-14 10:55:11 -0300

    Exercicio feito em encontro presencial Senai

---
+++ desafioPresencial2.js
@@ -0,0 +1,11 @@
+const alunos = 14;
+
+for(i = 0; i <= alunos; i++){
+    if(i==0){
+        console.log("Número Zero: " + i)
+    } else if(i % 2){
+        console.log("O Número é par: " + i)
+    } else{
+        console.log("O Número é impar: " + i)
+    }
+}
